902698f3d94c1630a40c02e18bfe6ddaf1af3c9a
Ricardo <ricardo.bragana@edu.unifor.br>
2018-07-12 15:09:00 -0300

    Added Thread Sample

---
+++ src/main/java/totalcross/sample/components/lang/thread/HTTPContainer.java
@@ -0,0 +1,95 @@
+package totalcross.sample.components.lang.thread;
+
+import totalcross.io.IOException;
+import totalcross.net.Socket;
+import totalcross.sys.Vm;
+import totalcross.ui.Container;
+import totalcross.ui.Label;
+import totalcross.ui.MultiEdit;
+import totalcross.ui.gfx.Color;
+
+public class HTTPContainer extends Container implements Runnable, ThreadSample.SetX {
+  MultiEdit me;
+  Label label;
+
+  @Override
+  public void initUI() {
+    setBackColor(Color.brighter(Color.CYAN));
+    setBorderStyle(BORDER_RAISED);
+
+    add(label = new Label(), LEFT, TOP, FILL, PREFERRED);
+    add(me = new MultiEdit(0, 0), LEFT, AFTER, FILL, FILL);
+    me.setEditable(false);
+    MultiEdit.hasCursorWhenNotEditable = false;
+    me.setBackColor(Color.CYAN);
+
+    Thread t = new Thread(this);
+    t.setPriority(3); // priority must be set BEFORE the thread is started! - palm os never puts working threads with priority 1 if there's another one with priority 5
+    t.start();
+  }
+
+  @Override
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  @Override
+  public void incX(int x) {
+    this.x += x;
+  }
+
+  @Override
+  public void run() {
+    Socket socket = null;
+    while (true) {
+      try {
+        me.setText("Content is Loading...");
+        if (ThreadSample.paused || ThreadSample.paused0) {
+          me.repaintNow();
+        }
+        socket = new Socket("www.google.com", 80, 25000);
+        socket.readTimeout = 5000;
+        String requestString = "GET /index.html HTTP/1.0\n\n";
+        byte[] get = requestString.getBytes();
+        socket.writeBytes(get);
+
+        int totalCount = 0;
+        int count = 0;
+
+        byte[] buff = new byte[100];
+        boolean done = false;
+        StringBuffer responseBuffer = new StringBuffer(64);
+        while (!done) {
+          count = socket.readBytes(buff, 0, buff.length);
+          String text = new String(buff, 0, count);
+          responseBuffer.append(text);
+          totalCount += count;
+          label.setText("Read " + totalCount + " from www.google.com");
+          if (ThreadSample.paused || ThreadSample.paused0) {
+            label.repaintNow();
+          }
+          if (count < buff.length) {
+            done = true;
+          }
+          Vm.sleep(50);
+        }
+        socket.close();
+
+        String googleHTML = responseBuffer.toString();
+        int index = googleHTML.indexOf('<');
+        if (index >= 0) {
+          googleHTML = googleHTML.substring(index);
+        }
+
+        me.setText(googleHTML);
+        if (ThreadSample.paused || ThreadSample.paused0) {
+          me.repaintNow();
+        }
+      } catch (IOException ioE) {
+        me.setText("IOException - " + ioE.getMessage());
+        me.repaintNow();
+      }
+      Vm.sleep(5000);
+    }
+  }
+}
---
+++ src/main/java/totalcross/sample/components/lang/thread/TypingContainer.java
@@ -0,0 +1,69 @@
+package totalcross.sample.components.lang.thread;
+
+import totalcross.sys.Vm;
+import totalcross.ui.Container;
+import totalcross.ui.MultiEdit;
+import totalcross.ui.gfx.Color;
+
+public class TypingContainer extends Container implements Runnable, ThreadSample.SetX {
+  String typingText = "The new virtual machine, called TotalCross, has better performance due to a new instruction set that eliminates limitations in the existing SuperWaba virtual machine, with enhancements such as unlimited object size, preemptive threads and a new high-performance garbage collector that is 20X faster than the SuperWaba's. Additionally, deployed files are now compacted, to acheive a 30% reduction in size over SuperWaba applications.";
+
+  int index = 0;
+  MultiEdit me;
+  boolean fill;
+
+  public TypingContainer(boolean fill) {
+    this.fill = fill;
+  }
+
+  @Override
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  @Override
+  public void incX(int x) {
+    this.x += x;
+  }
+
+  @Override
+  public void initUI() {
+    super.initUI();
+    setBackColor(Color.brighter(fill ? Color.YELLOW : Color.GREEN));
+    setBorderStyle(BORDER_RAISED);
+
+    me = new MultiEdit(0, 0);
+    add(me, LEFT, TOP, FILL, FILL);
+    if (fill) {
+      me.justify = fill;
+    }
+    me.setEditable(false);
+    MultiEdit.hasCursorWhenNotEditable = false;
+    me.setBackColor(fill ? Color.YELLOW : Color.GREEN);
+
+    Thread t = new Thread(this);
+    t.start();
+  }
+
+  @Override
+  public void run() {
+    int length = typingText.length();
+    StringBuffer sb = new StringBuffer(length);
+    while (true) {
+      index = 0;
+      while (index < length) {
+        sb.append(typingText.charAt(index));
+        me.setText(sb.toString());
+        if (ThreadSample.paused || ThreadSample.paused0) {
+          me.repaintNow();
+        }
+        index++;
+        Vm.sleep(100);
+      }
+      Vm.sleep(1000);
+      sb.setLength(0);
+      me.setText("");
+      index = 0;
+    }
+  }
+}
