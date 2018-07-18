3c864c0ecd005347b2dc62268384674705ea1c6a
Timbó <matheusfild4@hotmail.com>
2018-07-18 11:32:25 -0300

    Added 1 smaller switch button on switch sample

--- src/main/java/totalcross/sample/components/ui/SwitchSample.java
+++ src/main/java/totalcross/sample/components/ui/SwitchSample.java
@@ -1,25 +1,31 @@
 package totalcross.sample.components.ui;
 
+import totalcross.sample.util.Colors;
+import totalcross.sys.Settings;
 import totalcross.ui.Container;
 import totalcross.ui.Switch;
 import totalcross.ui.dialog.MessageBox;
 import totalcross.ui.event.ControlEvent;
 import totalcross.ui.event.PressListener;
+import totalcross.ui.gfx.Color;
 
 public class SwitchSample extends Container implements PressListener {
 	Switch s1, s2;
+	Switch s3;
 
 	@Override
 	public void initUI() {
 		try {
 			super.initUI();
+			s3 = new Switch(true);
+			add(s3, CENTER, PARENTSIZE+30, Settings.screenWidth/7, PREFERRED+fmH);
 			s1 = new Switch(true);
-			add(s1, CENTER, PARENTSIZE + 30, PARENTSIZE + 50, PREFERRED + fmH);
+			add(s1, CENTER, AFTER + Settings.screenHeight/10, PARENTSIZE + 50, PREFERRED + fmH);
 			s1 = new Switch(true);
 			s1.setFont(font.adjustedBy(fmH / 2));
-			add(s1, CENTER, AFTER + fmH, PARENTSIZE + 50, PREFERRED + fmH);
+			add(s1, CENTER, AFTER + Settings.screenHeight/10, PARENTSIZE + 50, PREFERRED + fmH);
 			s2 = new Switch(false);
-			add(s2, CENTER, AFTER + fmH, PARENTSIZE + 60, PREFERRED + fmH);
+			add(s2, CENTER, AFTER + Settings.screenHeight/10, PARENTSIZE + 60, PREFERRED + fmH);
 			s1.addPressListener(this);
 			s2.addPressListener(this);
 			s1.textForeOn = "I";
