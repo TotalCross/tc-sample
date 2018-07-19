9a849ef4b35858a071e4328bdfab262b3b13bf0c
Ricardo <ricardo.bragana@edu.unifor.br>
2018-07-19 16:16:13 -0300

    Making the button sample easier to use

--- src/main/java/totalcross/sample/components/ui/ButtonSample.java
+++ src/main/java/totalcross/sample/components/ui/ButtonSample.java
@@ -1,6 +1,7 @@
 package totalcross.sample.components.ui;
 
 import totalcross.ui.Container;
+import totalcross.ui.Label;
 import totalcross.ui.MultiButton;
 import totalcross.ui.ScrollContainer;
 import totalcross.ui.gfx.Color;
@@ -27,21 +28,22 @@ public class ButtonSample extends Container{
 		super.initUI();
 
 		try {
-			
+			setBackColor(Color.WHITE);
 			sc = new ScrollContainer(false, true);
-		    add(sc,LEFT,TOP,FILL,FILL);
+			add(new Label("All the following buttons are\nonly for demonstration purposes.", CENTER), CENTER, TOP, PREFERRED, PREFERRED);
+		    add(sc, LEFT, AFTER + 5, FILL, FILL);
 			
 			simpleButton = new Button("Simple button");
 			simpleButton.setBackColor(Colors.P_DARK);
 			simpleButton.setForeColor(Color.WHITE);
 			
-			multiLineButton = new Button("This is\na multi-line\nButton");
+			multiLineButton = new Button("This is a multi-line button, you can add \nas much lines as ou want, as long as it's\nwithin the screen range.");
 			multiLineButton.setBackColor(Colors.GREEN);
 			multiLineButton.setForeColor(Color.WHITE);
 			
-			Image img = new Image("images/find.png").hwScaledFixedAspectRatio(fmH*2, true);			
+			Image img = new Image("images/logoV.png").hwScaledFixedAspectRatio(fmH*2, true);			
 		    
-		    imageButton = new Button("This is an image Button", img, RIGHT, 0);
+		    imageButton = new Button("  This is a text button with an image", img, RIGHT, 0);
 		    imageButton.setBackColor(Colors.RED);
 		    imageButton.setForeColor(Color.WHITE);
 		    
@@ -50,23 +52,23 @@ public class ButtonSample extends Container{
 		    simpleMultiButton.setForeColor(Color.WHITE);
 		    
 
-		    multiButton = new MultiButton(new String[]{"Left","Disabled","Right"});
+		    multiButton = new MultiButton(new String[]{"Left","Center","Right"});
 		    multiButton.setBackColor(Colors.PURPLE);
 		    multiButton.setForeColor(Color.WHITE);
 		    multiButton.isSticky = multiButton.is3dText = true;
-		    multiButton.setEnabled(1,false);
 		    
-		    Image logo = new Image("images/logoV.png").smoothScaledBy((double)480/2048, (double)480/2048); // Searching and scaling the image.
-		    onlyImageButton = new Button(logo); // This creates a button with an image only, no text.
+		    Image image = new Image("images/find.png").smoothScaledBy((double)480/2048, (double)480/2048); // Searching and scaling the image.
+		    onlyImageButton = new Button(image); // This creates a button with an image only, no text.
 		    onlyImageButton.setBorder(Button.BORDER_NONE); //This sets the button's border to null
 		    
 			
 			sc.add(simpleButton, LEFT+B, AFTER+B, FILL-B, PREFERRED+H);		
-			sc.add(multiLineButton, CENTER, AFTER+B, PREFERRED+B, PREFERRED+H);
+			sc.add(multiLineButton, CENTER, AFTER+B, SAME, PREFERRED+H);
 			sc.add(imageButton, LEFT+B, AFTER+B, FILL-B, PREFERRED+H);
 			sc.add(simpleMultiButton, LEFT+B , AFTER+B , FILL-B , PREFERRED+H);
 			sc.add(multiButton, LEFT+B , AFTER+B, FILL-B, PREFERRED+H);
-			sc.add(onlyImageButton, CENTER, AFTER+B, PREFERRED, PREFERRED);
+			sc.add(new Label("The magnifier\nis an Image\nButton", CENTER), LEFT + 150, AFTER + B, PREFERRED, PREFERRED);
+			sc.add(onlyImageButton, CENTER, SAME, PREFERRED, PREFERRED);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
