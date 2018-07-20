e827e1cd79985779f4ccf31db2cd287ce0ff1b07
Timbó <matheusfild4@hotmail.com>
2018-07-20 14:36:14 -0300

    Readd MailSample Changes

--- src/main/java/totalcross/sample/components/net/mail/ConfigurationContainer.java
+++ src/main/java/totalcross/sample/components/net/mail/ConfigurationContainer.java
@@ -1,132 +1,135 @@
+package totalcross.sample.components.net.mail;
  
-package totalcross.sample.components.net.mail; 
+import totalcross.io.DataStream;
+import totalcross.io.File;
+import totalcross.net.mail.MailSession;
+import totalcross.sys.Convert;
+import totalcross.sys.InvalidNumberException;
+import totalcross.sys.Settings;
+import totalcross.ui.Button;
+import totalcross.ui.Edit;
+import totalcross.ui.Label;
+import totalcross.ui.Window;
+import totalcross.ui.dialog.MessageBox;
+import totalcross.ui.event.ControlEvent;
+import totalcross.ui.event.Event;
+import totalcross.ui.gfx.Color;
+import totalcross.util.Properties;
+import totalcross.util.Properties.Value;
  
-import totalcross.io.DataStream; 
-import totalcross.io.File; 
-import totalcross.net.mail.MailSession; 
-import totalcross.sys.Convert; 
-import totalcross.sys.InvalidNumberException; 
-import totalcross.sys.Settings; 
-import totalcross.ui.Button; 
-import totalcross.ui.Edit; 
-import totalcross.ui.Label; 
-import totalcross.ui.Window; 
-import totalcross.ui.dialog.MessageBox; 
-import totalcross.ui.event.ControlEvent; 
-import totalcross.ui.event.Event; 
-import totalcross.util.Properties; 
-import totalcross.util.Properties.Value; 
+public class ConfigurationContainer extends Window {
+  Edit edLogin;
+  Edit edPassword;
+  Edit edPopHost;
+  Edit edPopPort;
+  Edit edSmtpHost;
+  Edit edSmtpPort;
  
-public class ConfigurationContainer extends Window { 
-  Edit edLogin; 
-  Edit edPassword; 
-  Edit edPopHost; 
-  Edit edPopPort; 
-  Edit edSmtpHost; 
-  Edit edSmtpPort; 
+  Button btOk;
  
-  Button btOk; 
+  MailSession mp = MailSession.getDefaultInstance();
+  String filePath = Settings.appPath + "/" + "MailTestConfig.dat";
+  boolean loaded;
  
-  MailSession mp = MailSession.getDefaultInstance(); 
-  String filePath = Settings.appPath + "/" + "MailTestConfig.dat"; 
-  boolean loaded; 
+  public ConfigurationContainer() {
+    super("Configuration", ROUND_BORDER);
+    try {
+      File f = new File(filePath, File.CREATE);
+      if (f.getSize() > 0) {
+        mp.load(new DataStream(f));
+      }
+      if (mp.size() > 0) { loaded = true;
+      }
+      f.close();
+    } catch (Exception e) {
+      MessageBox.showException(e, true);
+    }
+    setRect(CENTER, CENTER, (Settings.screenWidth/10)*9, fmH * 10);
+    this.setBackColor(Color.getRGB(74, 144, 226));
+    this.setForeColor(Color.WHITE);
+  }
  
-  public ConfigurationContainer() { 
-    super("Configuration", ROUND_BORDER); 
-    try { 
-      File f = new File(filePath, File.CREATE); 
-      if (f.getSize() > 0) { 
-        mp.load(new DataStream(f)); 
-      } 
-      if (mp.size() > 0) { 
-        loaded = true; 
-      } 
-      f.close(); 
-    } catch (Exception e) { 
-      MessageBox.showException(e, true); 
-    } 
-    setRect(CENTER, CENTER, SCREENSIZE + 80, fmH * 10); 
-  } 
+  @Override
+  public void initUI() {
+  this.setBackColor(Color.getRGB(74, 144, 226));
+  this.setForeColor(Color.WHITE);
+    add(new Label("Login: "), LEFT + Settings.screenWidth/10, TOP, PARENTSIZE + 40, PREFERRED);
+    add(edLogin = new Edit(), AFTER, SAME);
  
-  @Override 
-  public void initUI() { 
-    add(new Label("Login: "), LEFT, TOP, PARENTSIZE + 40, PREFERRED); 
-    add(edLogin = new Edit(), AFTER, SAME); 
+    add(new Label("Password: "), LEFT + Settings.screenWidth/10 , AFTER, PARENTSIZE + 40, PREFERRED);
+    add(edPassword = new Edit(), AFTER, SAME);
+    edPassword.setMode(Edit.PASSWORD);
  
-    add(new Label("Password: "), LEFT, AFTER, PARENTSIZE + 40, PREFERRED); 
-    add(edPassword = new Edit(), AFTER, SAME); 
-    edPassword.setMode(Edit.PASSWORD); 
+    add(new Label("SMTP host: "), LEFT + Settings.screenWidth/10, AFTER, PARENTSIZE + 40, PREFERRED);
+    add(edSmtpHost = new Edit(), AFTER, SAME);
  
-    add(new Label("SMTP host: "), LEFT, AFTER, PARENTSIZE + 40, PREFERRED); 
-    add(edSmtpHost = new Edit(), AFTER, SAME); 
+    add(new Label("SMTP port: "), LEFT + Settings.screenWidth/10, AFTER, PARENTSIZE + 40, PREFERRED);
+    add(edSmtpPort = new Edit("12345"), AFTER, SAME);
+    edSmtpPort.setValidChars("0123456789");
+    edSmtpPort.setMaxLength(5);
+    edSmtpPort.setText("25");
  
-    add(new Label("SMTP port: "), LEFT, AFTER, PARENTSIZE + 40, PREFERRED); 
-    add(edSmtpPort = new Edit("12345"), AFTER, SAME); 
-    edSmtpPort.setValidChars("0123456789"); 
-    edSmtpPort.setMaxLength(5); 
-    edSmtpPort.setText("25"); 
+    add(new Label("POP3 host: "), LEFT + Settings.screenWidth/10, AFTER, PARENTSIZE + 40, PREFERRED);
+    add(edPopHost = new Edit(), AFTER, SAME);
  
-    add(new Label("POP3 host: "), LEFT, AFTER, PARENTSIZE + 40, PREFERRED); 
-    add(edPopHost = new Edit(), AFTER, SAME); 
+    add(new Label("POP3 port: "), LEFT + Settings.screenWidth/10, AFTER, PARENTSIZE + 40, PREFERRED);
+    add(edPopPort = new Edit("12345"), AFTER, SAME);
+    edPopPort.setValidChars("0123456789");
+    edPopPort.setMaxLength(5);
+    edPopPort.setText("110");
+    add(btOk = new Button(" Ok "), RIGHT, BOTTOM);
+    
+    
+    if (loaded) {
+      Value value;
+      if ((value = mp.get(MailSession.POP3_USER)) != null) {
+        edLogin.setText(value.toString());
+      }
+      if ((value = mp.get(MailSession.POP3_PASS)) != null) {
+        edPassword.setText(value.toString());
+      }
+      if ((value = mp.get(MailSession.SMTP_HOST)) != null) {
+        edSmtpHost.setText(value.toString());
+      }
+      if ((value = mp.get(MailSession.SMTP_PORT)) != null) {
+        edSmtpPort.setText(value.toString());
+      }
+      if ((value = mp.get(MailSession.POP3_HOST)) != null) {
+        edPopHost.setText(value.toString());
+      }
+      if ((value = mp.get(MailSession.POP3_PORT)) != null) {
+        edPopPort.setText(value.toString());
+      }
+    }
+  }
+  @Override
+  public void onEvent(Event event) {
+    if (event.type == ControlEvent.PRESSED && event.target == btOk) {
+      mp.put(MailSession.SMTP_USER, new Properties.Str(edLogin.getText()));
+      mp.put(MailSession.POP3_USER, new Properties.Str(edLogin.getText()));
+      mp.put(MailSession.SMTP_PASS, new Properties.Str(edPassword.getText()));
+      mp.put(MailSession.POP3_PASS, new Properties.Str(edPassword.getText()));
  
-    add(new Label("POP3 port: "), LEFT, AFTER, PARENTSIZE + 40, PREFERRED); 
-    add(edPopPort = new Edit("12345"), AFTER, SAME); 
-    edPopPort.setValidChars("0123456789"); 
-    edPopPort.setMaxLength(5); 
-    edPopPort.setText("110"); 
+      mp.put(MailSession.SMTP_HOST, new Properties.Str(edSmtpHost.getText()));
+      mp.put(MailSession.POP3_HOST, new Properties.Str(edPopHost.getText()));
  
-    add(btOk = new Button(" Ok "), RIGHT, BOTTOM); 
+      try {
+        mp.put(MailSession.SMTP_PORT, new Properties.Int(Convert.toInt(edSmtpPort.getText())));
+        mp.put(MailSession.POP3_PORT, new Properties.Int(Convert.toInt(edPopPort.getText())));
+      } catch (InvalidNumberException e) {
+        // Should never happen, since the user may only use numbers for the port.
+        MessageBox.showException(e, true);
+      }
  
-    if (loaded) { 
-      Value value; 
-      if ((value = mp.get(MailSession.POP3_USER)) != null) { 
-        edLogin.setText(value.toString()); 
-      } 
-      if ((value = mp.get(MailSession.POP3_PASS)) != null) { 
-        edPassword.setText(value.toString()); 
-      } 
-      if ((value = mp.get(MailSession.SMTP_HOST)) != null) { 
-        edSmtpHost.setText(value.toString()); 
-      } 
-      if ((value = mp.get(MailSession.SMTP_PORT)) != null) { 
-        edSmtpPort.setText(value.toString()); 
-      } 
-      if ((value = mp.get(MailSession.POP3_HOST)) != null) { 
-        edPopHost.setText(value.toString()); 
-      } 
-      if ((value = mp.get(MailSession.POP3_PORT)) != null) { 
-        edPopPort.setText(value.toString()); 
-      } 
-    } 
-  } 
+      try {
+        File f = new File(filePath, File.CREATE_EMPTY);
+        mp.save(new DataStream(f));
+        f.close();
+      } catch (Exception e) {
+        MessageBox.showException(e, true);
+      }
+      unpop();
+    }
+  }
+}
  
-  @Override 
-  public void onEvent(Event event) { 
-    if (event.type == ControlEvent.PRESSED && event.target == btOk) { 
-      mp.put(MailSession.SMTP_USER, new Properties.Str(edLogin.getText())); 
-      mp.put(MailSession.POP3_USER, new Properties.Str(edLogin.getText())); 
-      mp.put(MailSession.SMTP_PASS, new Properties.Str(edPassword.getText())); 
-      mp.put(MailSession.POP3_PASS, new Properties.Str(edPassword.getText())); 
- 
-      mp.put(MailSession.SMTP_HOST, new Properties.Str(edSmtpHost.getText())); 
-      mp.put(MailSession.POP3_HOST, new Properties.Str(edPopHost.getText())); 
- 
-      try { 
-        mp.put(MailSession.SMTP_PORT, new Properties.Int(Convert.toInt(edSmtpPort.getText()))); 
-        mp.put(MailSession.POP3_PORT, new Properties.Int(Convert.toInt(edPopPort.getText()))); 
-      } catch (InvalidNumberException e) { 
-        // Should never happen, since the user may only use numbers for the port. 
-        MessageBox.showException(e, true); 
-      } 
- 
-      try { 
-        File f = new File(filePath, File.CREATE_EMPTY); 
-        mp.save(new DataStream(f)); 
-        f.close(); 
-      } catch (Exception e) { 
-        MessageBox.showException(e, true); 
-      } 
-      unpop(); 
-    } 
-  } 
-} 
--- src/main/java/totalcross/sample/components/net/mail/MailSample.java
+++ src/main/java/totalcross/sample/components/net/mail/MailSample.java
@@ -1,134 +1,154 @@
-package totalcross.sample.components.net.mail; 
- 
-import totalcross.net.AuthenticationException; 
-import totalcross.net.mail.Folder; 
-import totalcross.net.mail.MailSession; 
-import totalcross.net.mail.Message; 
-import totalcross.net.mail.MessagingException; 
-import totalcross.net.mail.Store; 
-import totalcross.ui.Button; 
-import totalcross.ui.Container; 
-import totalcross.ui.Control; 
-import totalcross.ui.Grid; 
-import totalcross.ui.Label; 
-import totalcross.ui.dialog.MessageBox; 
-import totalcross.ui.event.ControlEvent; 
-import totalcross.ui.event.Event; 
- 
-/** 
-Although not implemented here, here's now to send email to a GMAIL account: 
-<pre> 
-   Message m = new Message(); 
-   m.addFrom(new Address[] { new Address("me@gmail.com", "Me") }); 
-   m.addRecipient(RecipientType.TO, new Address("you@gmail.com", "You")); 
-   m.subject = "me and you"; 
-   m.setText("we're good friends"); 
- 
-   MailSession s = MailSession.getDefaultInstance(); 
-   s.put(MailSession.SMTP_HOST, new Properties.Str("smtp.gmail.com")); 
-   s.put(MailSession.SMTP_USER, new Properties.Str("postmaster@gmail.com")); 
-   s.put(MailSession.SMTP_PASS, new Properties.Str("a good password!)); 
-   s.put(MailSession.SMTP_PORT, new Properties.Int(587)); 
-   s.put(MailSession.SMTP_AUTH, new Properties.Boolean(true)); 
-   s.put(MailSession.SMTP_STARTTLS, new Properties.Boolean(true)); 
-   SMTPSSLTransport.send(m, s); 
-</pre> 
-If you receive error 534, see: 
-https://accounts.google.com/b/0/DisplayUnlockCaptcha 
- 
- */ 
- 
-public class MailSample extends Container { 
-  MessageContainer messagePanel; 
- 
-  Label lblInbox; 
-  Label lblPage; 
-  Button btDelete; 
-  Button btReset; 
-  Button btOpen; 
-  Button btConnect; 
-  Grid inbox; 
- 
-  Store store; 
-  Folder folder; 
- 
-  MailboxDataSource mds; 
- 
-  @Override 
-  public void initUI() { 
-    super.initUI(); 
-    add(lblPage = new Label("Inbox", Control.CENTER), CENTER, TOP); 
-    add(btConnect = new Button("Synchronize"), RIGHT, TOP); 
-    add(btReset = new Button("Reset"), LEFT, BOTTOM); 
-    add(btOpen = new Button("Open"), CENTER, BOTTOM); 
-    Grid.useHorizontalScrollBar = true; 
-    inbox = new Grid(new String[] { "Subject" }, new int[] { -400 }, new int[] { LEFT }, false); 
-    add(inbox, LEFT, AFTER, FILL, FIT, lblPage); 
- 
-    new ConfigurationContainer().popup(); 
-  } 
- 
-  Message[] msgs; 
- 
-  @Override 
-  public void onEvent(Event event) { 
-    try { 
-      if (event.target == btReset && event.type == ControlEvent.PRESSED) { 
-        inbox.removeAllElements(); 
-        folder.reset(); 
-        inbox.setDataSource(mds, folder.getMessageCount()); 
-      } else if (event.target == btOpen && event.type == ControlEvent.PRESSED) { 
-        int selectedIndex = inbox.getSelectedIndex(); 
-        if (selectedIndex != -1) { 
-          String[] item = inbox.getSelectedItem(); 
-          showMessage(folder.getMessage(item[1])); 
-        } 
-      } else if (event.target == btConnect && event.type == ControlEvent.PRESSED) { 
-        connect(); 
+
+package totalcross.sample.components.net.mail;
+
+import totalcross.net.AuthenticationException;
+import totalcross.net.mail.Folder;
+import totalcross.net.mail.MailSession;
+import totalcross.net.mail.Message;
+import totalcross.net.mail.MessagingException;
+import totalcross.net.mail.Store;
+import totalcross.sys.Settings;
+import totalcross.ui.Button;
+import totalcross.ui.Container;
+import totalcross.ui.Control;
+import totalcross.ui.Grid;
+import totalcross.ui.Label;
+import totalcross.ui.dialog.MessageBox;
+import totalcross.ui.event.ControlEvent;
+import totalcross.ui.event.Event;
+import totalcross.ui.gfx.Color;
+
+/**
+Although not implemented here, here's now to send email to a GMAIL account:
+<pre>
+   Message m = new Message();
+   m.addFrom(new Address[] { new Address("me@gmail.com", "Me") });
+   m.addRecipient(RecipientType.TO, new Address("you@gmail.com", "You"));
+   m.subject = "me and you";
+   m.setText("we're good friends");
+
+   MailSession s = MailSession.getDefaultInstance();
+   s.put(MailSession.SMTP_HOST, new Properties.Str("smtp.gmail.com"));
+   s.put(MailSession.SMTP_USER, new Properties.Str("postmaster@gmail.com"));
+   s.put(MailSession.SMTP_PASS, new Properties.Str("a good password!));
+   s.put(MailSession.SMTP_PORT, new Properties.Int(587));
+   s.put(MailSession.SMTP_AUTH, new Properties.Boolean(true));
+   s.put(MailSession.SMTP_STARTTLS, new Properties.Boolean(true));
+   SMTPSSLTransport.send(m, s);
+</pre>
+If you receive error 534, see:
+https://accounts.google.com/b/0/DisplayUnlockCaptcha
+
+ */
+
+public class MailSample extends Container {
+  MessageContainer messagePanel;
+
+  Label lblInbox;
+  Label lblPage;
+  Button btDelete;
+  Button btReset;
+  Button btOpen;
+  Button btConnect;
+  Button btConfig;
+  Grid inbox;
+
+  Store store;
+  Folder folder;
+
+  MailboxDataSource mds;
+
+  @Override
+  public void initUI() {
+    super.initUI();
+    this.setBackColor(Color.getRGB(74, 144, 226));
+    add(btConfig = new Button("Configuration"), LEFT + ((Settings.screenWidth/100)*8), TOP,(Settings.screenWidth/10)*4, 25);
+    btConfig.setBackColor(Color.getRGB(69, 131, 212));
+    btConfig.setForeColor(Color.WHITE);
+    add(lblPage = new Label("Inbox", Control.CENTER), CENTER, TOP + ((Settings.screenWidth/100)*8));
+    lblPage.setForeColor(Color.WHITE);
+    add(btConnect = new Button("Synchronize"), RIGHT - ((Settings.screenWidth/100)*8), TOP, (Settings.screenWidth/10)*4, 25);
+    btConnect.setBackColor(Color.getRGB(69, 131, 212));
+    btConnect.setForeColor(Color.WHITE);
+    add(btReset = new Button("Reset"), LEFT + ((Settings.screenWidth/100)*8) ,BOTTOM-(Settings.screenHeight/10), (Settings.screenWidth/10)*4, 25);
+    btReset.setBackColor(Color.getRGB(69, 131, 212));
+    btReset.setForeColor(Color.WHITE);
+    add(btOpen = new Button("Open"), RIGHT -((Settings.screenWidth/100)*8), BOTTOM-(Settings.screenHeight/10), (Settings.screenWidth/10)*4, 25);
+    btOpen.setBackColor(Color.getRGB(69, 131, 212));
+    btOpen.setForeColor(Color.WHITE);
+    Grid.useHorizontalScrollBar = true;
+    inbox = new Grid(new String[] { "Email","Subject" }, new int[] { Settings.screenWidth/2, Settings.screenWidth/2 }, new int[] { LEFT, LEFT }, false);
+    inbox.firstStripeColor = totalcross.ui.gfx.Color.getRGB(255, 255, 255);
+    inbox.secondStripeColor = totalcross.ui.gfx.Color.getRGB(255, 255, 255);
+    inbox.highlightColor = totalcross.ui.gfx.Color.getRGB(224, 224, 224);
+    inbox.verticalLineStyle = Grid.VERT_LINE;
+    add(inbox, LEFT, AFTER+Settings.screenHeight/10, FILL, FIT, lblPage);
+
+  }
+
+  Message[] msgs;
+
+  @Override
+  public void onEvent(Event event) {
+    try {
+      if (event.target == btReset && event.type == ControlEvent.PRESSED) {
+        inbox.removeAllElements();
+        folder.reset();
+        inbox.setDataSource(mds, folder.getMessageCount());
+      } else if (event.target == btOpen && event.type == ControlEvent.PRESSED) {
+        int selectedIndex = inbox.getSelectedIndex();
+        if (selectedIndex != -1) {
+          String[] item = inbox.getSelectedItem();
+          showMessage(folder.getMessage(item[1]));
+        }
+      } else if (event.target == btConnect && event.type == ControlEvent.PRESSED) {
+        connect();
+      } else if(event.target == btConfig && event.type == ControlEvent.PRESSED) {
+    	new ConfigurationContainer().popup();
       } 
-    } catch (MessagingException e) { 
-      MessageBox.showException(e, true); 
-    } 
-  } 
- 
-  private void connect() { 
-    if (store != null) { 
-      return; 
-    } 
- 
-    try { 
-      store = MailSession.getDefaultInstance().getStore("pop3"); 
-      store.connect(); 
-      folder = store.getFolder("INBOX"); 
-      folder.open(); 
-      inbox.liveScrolling = true; 
-      int messageCount = folder.getMessageCount(); 
-      inbox.setDataSource(mds = new MailboxDataSource(folder), messageCount); 
-      inbox.scrollTo(messageCount); 
-    } catch (AuthenticationException e) { 
-      MessageBox.showException(e, true); 
-    } catch (MessagingException e) { 
-      MessageBox.showException(e, true); 
-    } 
-  } 
- 
-  private void showMessage(Message message) { 
-    if (message != null) { 
-      new MessageContainer(message).popup(); 
-    } 
-  } 
- 
-  @Override 
-  public void onRemove() { 
-    try { 
-      if (folder != null) { 
-        folder.close(false); 
-      } 
-      if (store != null) { 
-        store.close(); 
-      } 
-    } catch (MessagingException e) { 
-      MessageBox.showException(e, true); 
-    } 
-  } 
-} 
+    } catch (MessagingException e) {
+      MessageBox.showException(e, true);
+    }
+  }
+
+  private void connect() {
+    if (store != null) {
+      return;
+    }
+
+    try {
+      store = MailSession.getDefaultInstance().getStore("pop3");
+      store.connect();
+      folder = store.getFolder("INBOX");
+      folder.open();
+      inbox.liveScrolling = true;
+      int messageCount = folder.getMessageCount();
+      inbox.setDataSource(mds = new MailboxDataSource(folder), messageCount);
+      inbox.scrollTo(messageCount);
+    } catch (AuthenticationException e) {
+      MessageBox.showException(e, true);
+    } catch (MessagingException e) {
+      MessageBox.showException(e, true);
+    }
+  }
+
+  private void showMessage(Message message) {
+    if (message != null) {
+      new MessageContainer(message).popup();
+    }
+  }
+
+  @Override
+  public void onRemove() {
+    try {
+      if (folder != null) {
+        folder.close(false);
+      }
+      if (store != null) {
+        store.close();
+      }
+    } catch (MessagingException e) {
+      MessageBox.showException(e, true);
+    }
+  }
+}
