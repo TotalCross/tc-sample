27bbb724b9ab505a842d3c7124f9850426dea356
João Ferreira <joaot10112@gmail.com>
2018-07-27 16:50:07 -0300

    Visual and code improvements on ProgressBox sample

--- src/main/java/totalcross/sample/components/ui/ProgressBoxSample.java
+++ src/main/java/totalcross/sample/components/ui/ProgressBoxSample.java
@@ -1,5 +1,6 @@
 package totalcross.sample.components.ui;
 
+import totalcross.sample.util.Colors;
 import totalcross.sys.Vm;
 import totalcross.ui.ButtonMenu;
 import totalcross.ui.Container;
@@ -8,57 +9,59 @@ import totalcross.ui.dialog.MessageBox;
 import totalcross.ui.dialog.ProgressBox;
 import totalcross.ui.event.ControlEvent;
 import totalcross.ui.event.Event;
+import totalcross.ui.event.PressListener;
 import totalcross.ui.gfx.Color;
 
 public class ProgressBoxSample extends Container {
-  ButtonMenu menu;
+  private ButtonMenu menu;
+  private ProgressBox pb;
+  private int count;
 
   @Override
   public void initUI() {
     try {
       super.initUI();
-
-      String[] items = { "Small ProgressBox (style 1)", "Big ProgressBox (style 1)", "Small ProgressBox (style 2)",
-          "Big ProgressBox (style 2)" };
+      pb = new ProgressBox("Alert!", "null", null);
+      
+      String[] items = {"ProgressBox (Android style)", "ProgressBox (iOS style)"};
 
       menu = new ButtonMenu(items, ButtonMenu.SINGLE_COLUMN);
+      menu.borderGap = 30;
+      menu.textGap = 40;
+      
       menu.pressedColor = Color.GREEN;
       add(menu, LEFT, TOP, FILL, FILL);
 
-      //setInfo("Each test takes 5 seconds");
+      count = 2;
+      
+      for (int i = 0; i < items.length; i++)
+    	  menu.getButton(i).setBackForeColors(Colors.P_DARK, Color.WHITE);
+      
+      
+      menu.addPressListener((e) -> {
+    	  int sel = menu.getSelectedIndex();
+          Spinner.spinnerType = sel == 0 ? Spinner.ANDROID : Spinner.IPHONE;
+          
+          pb = new ProgressBox("Alert!", "Please wait\n" + count + "seconds", null);
+          pb.setBackColor(Color.getRGB(12, 98, 200));
+          menu.addTimer(1000);
+          pb.popup();
+      });
+      
+      menu.addTimerListener((e) -> {
+    	  System.out.println("TESTE");
+    	  if(count >= 0) {
+    		  pb.setText("Please wait\n" + count + " seconds.");
+    		  count--;
+    	  } else {
+    		  pb.unpop();
+    		  count = 2;
+    		  menu.removeTimer(e);
+    	  }
+      });
+      
     } catch (Exception ee) {
       MessageBox.showException(ee, true);
-    }
-  }
-
-  @Override
-  public void onEvent(Event e) {
-    if (e.type == ControlEvent.PRESSED && e.target == menu) {
-      int sel = menu.getSelectedIndex();
-      Spinner.spinnerType = sel >= 2 ? Spinner.ANDROID : Spinner.IPHONE;
-      String msg = sel == 0 || sel == 2 ? "Please wait\n 2 seconds."
-          : "Please wait\n 4 seconds.";
-      ProgressBox pb = new ProgressBox("Alert!", msg, null);
-      pb.setBackColor(Color.getRGB(12, 98, 200));
-      pb.popupNonBlocking();
-      // we can't just block using Vm.sleep because it would also 
-      // block a screen rotation from correctly paint the screen
-      if(sel == 1 || sel == 3) {
-    	for(int i=3; i>0; i--) {
-    		Vm.safeSleep(1000);
-    		if(i!=1)
-    		pb.setText("Please wait\n " +i+ " seconds.");
-    		else
-    		pb.setText("Please wait\n " +i+ " seconds.");
-    			
-    	}
-      }
-      else
-    	  Vm.safeSleep(1000);
-          pb.setText("Please wait\n 1 second.");
-          Vm.safeSleep(1000);
-      pb.unpop();
-      //setInfo(sel == 1 || sel == 3 ? "BUM!!!!" : "Loaded");
     }
   }
 }
