7d28948c80595c7a44592107e19c822aa27613d0
João Ferreira <joaot10112@gmail.com>
2018-08-06 18:58:30 -0300

    ZLib sample working properly

--- src/main/java/totalcross/sample/components/util/ZLibSample.java
+++ src/main/java/totalcross/sample/components/util/ZLibSample.java
@@ -20,104 +20,110 @@ import totalcross.io.ByteArrayStream;
 import totalcross.io.IOException;
 import totalcross.sample.util.Colors;
 import totalcross.sys.Convert;
+import totalcross.sys.Settings;
 import totalcross.sys.Vm;
 import totalcross.ui.Button;
-import totalcross.ui.Container;
+import totalcross.ui.Label;
+import totalcross.ui.ScrollContainer;
 import totalcross.ui.event.ControlEvent;
 import totalcross.ui.event.Event;
+import totalcross.ui.gfx.Color;
 import totalcross.util.zip.ZLib;
 
-public class ZLibSample extends Container {
-  private Button btn;
+public class ZLibSample extends ScrollContainer {
+	private Button btn;
+	private int gap = (int) (Settings.screenDensity * 10);
+	@Override
+	public void initUI() {
+		super.initUI();
+		setBackForeColors(Colors.BACKGROUND, Colors.ON_BACKGROUND);
+		add(btn = new Button("    Start    "), CENTER, TOP + 3);
+	}
 
-  @Override
-  public void initUI() {
-    super.initUI();
-    setBackForeColors(Colors.BACKGROUND, Colors.ON_BACKGROUND);
-    add(btn = new Button("    Start    "), CENTER, TOP + 3);
-    //addLog(LEFT, AFTER + 3, FILL, FILL, null);
-  }
+	@Override
+	public void onEvent(Event e) {
+		if (e.type == ControlEvent.PRESSED && e.target == btn) {
+			add(new Label("proceed(BEST_COMPRESSION)", LEFT, Color.BLACK, true), LEFT + gap, AFTER + gap);
+			proceed(ZLib.BEST_COMPRESSION);
+			
+			add(new Label("proceed(BEST_SPEED)", LEFT, Color.BLACK, true), LEFT + gap, AFTER + gap);
+			proceed(ZLib.BEST_SPEED);
+		}
+	}
 
-  @Override
-  public void onEvent(Event e) {
-    if (e.type == ControlEvent.PRESSED && e.target == btn) {
-      //log("----------- proceed(BEST_COMPRESSION) -----------");
-      proceed(ZLib.BEST_COMPRESSION);
+	private void proceed(int compression_level) {
+		int sz = 0;
+		StringBuffer sb = new StringBuffer(42 * 500);
 
-      //log("----------- proceed(BEST_SPEED) -----------");
-      proceed(ZLib.BEST_SPEED);
-    }
-  }
+		for (int i = 0; i < 500; i++) {
+			sb = sb.append("This is the uncompressed original message.");
+		}
+		
+		add(new Label("original size is " + sb.length()), LEFT + gap, AFTER + gap);
 
-  private void proceed(int compression_level) {
-    int sz = 0;
-    StringBuffer sb = new StringBuffer(42 * 500);
+		ByteArrayStream is = new ByteArrayStream(Convert.getBytes(sb));
+		ByteArrayStream cs = new ByteArrayStream(0);
 
-    for (int i = 0; i < 500; i++) {
-      sb = sb.append("This is the uncompressed original message.");
-    }
+		try {
+			int ini = Vm.getTimeStamp();
+			sz = ZLib.deflate(is, cs, compression_level);
+			int end = Vm.getTimeStamp();
+			add(new Label("compressed output size=" + sz), LEFT + gap, AFTER + gap);
+			add(new Label("Elapsed: " + (end - ini) + "ms"), LEFT + gap, AFTER + gap);
 
-    //log("original size is " + sb.length());
+		} catch (IOException e) {
+			add(new Label("an exception occurred : " + e.getMessage(), LEFT, Colors.ERROR, true), LEFT + gap, AFTER + gap);
+		}
+		try {
+			cs.setPos(0);
+		} catch (IOException exc) {
+			add(new Label("an exception occurred : " + exc.getMessage(), LEFT, Colors.ERROR, true), LEFT + gap, AFTER + gap);
+		}
+		add(new Label("compressed byte stream:"), LEFT + gap, AFTER + gap);
 
-    ByteArrayStream is = new ByteArrayStream(Convert.getBytes(sb));
-    ByteArrayStream cs = new ByteArrayStream(0);
+		byte buf[] = new byte[8];
+		int read, ofs = 0, left = sz;
+		while (left > 0) {
+			read = left;
+			if (read > buf.length) {
+				read = buf.length;
+			}
 
-    try {
-      int ini = Vm.getTimeStamp();
-      sz = ZLib.deflate(is, cs, compression_level);
-      int end = Vm.getTimeStamp();
-      //log("compressed output size=" + sz);
-      //log("Elapsed: " + (end - ini) + "ms");
-    } catch (IOException e) {
-      //log("an exception occurred : " + e.getMessage());
-    }
-    try {
-      cs.setPos(0);
-    } catch (IOException exc) {
-      //log("an exception occurred : " + exc.getMessage());
-    }
+			read = cs.readBytes(buf, 0, read);
+			left -= read;
 
-    //log("compressed byte stream:");
-    byte buf[] = new byte[8];
-    int read, ofs = 0, left = sz;
-    while (left > 0) {
-      read = left;
-      if (read > buf.length) {
-        read = buf.length;
-      }
+			sb.setLength(0);
+			for (int i = 0; i < read; i++) {
+				sb = sb.append(' ').append(Convert.unsigned2hex(buf[i], 2));
+			}
+			add(new Label(ofs + " - " + sb.toString()), LEFT + gap, AFTER + gap);
 
-      read = cs.readBytes(buf, 0, read);
-      left -= read;
+			ofs += read;
+		}
+		ByteArrayStream os = new ByteArrayStream(0);
 
-      sb.setLength(0);
-      for (int i = 0; i < read; i++) {
-        sb = sb.append(' ').append(Convert.unsigned2hex(buf[i], 2));
-      }
-      //log(ofs + " - " + sb.toString());
-      ofs += read;
-    }
-    ByteArrayStream os = new ByteArrayStream(0);
+		try {
+			cs.setPos(0);
+		} catch (IOException exc) {
+			add(new Label("an exception occurred : " + exc.getMessage(), LEFT, Colors.ERROR, true), LEFT + gap, AFTER + gap);
+		}
+		try {
+			int ini = Vm.getTimeStamp();
+			sz = ZLib.inflate(cs, os, sz);
+			int end = Vm.getTimeStamp();
+			
+			add(new Label("uncompressed output size=" + sz), LEFT + gap, AFTER + gap);
+			add(new Label("Elapsed: " + (end - ini) + "ms"), LEFT + gap, AFTER + gap);
 
-    try {
-      cs.setPos(0);
-    } catch (IOException exc) {
-      //log("an exception occurred : " + exc.getMessage());
-    }
-    try {
-      int ini = Vm.getTimeStamp();
-      sz = ZLib.inflate(cs, os, sz);
-      int end = Vm.getTimeStamp();
-      //log("uncompressed output size=" + sz);
-      //log("Elapsed: " + (end - ini) + "ms");
-    } catch (IOException e) {
-      //log("an exception occurred : " + e.getMessage());
-    }
+		} catch (IOException e) {
+			add(new Label("an exception occurred : " + e.getMessage(), LEFT, Colors.ERROR, true), LEFT + gap, AFTER + gap);
+		}
 
-    if (sz > 0) {
-      String str = new String(os.getBuffer(), 0, sz);
-
-      //log("original content was '" + str.substring(0, 20) + "'");
-      //log("original size was " + sz);
-    }
-  }
+		if (sz > 0) {
+			String str = new String(os.getBuffer(), 0, sz);
+			
+			add(new Label("original content was '" + str.substring(0, 20) + "'"), LEFT + gap, AFTER + gap);
+			add(new Label("original size was " + sz), LEFT + gap, AFTER + gap);
+		}
+	}
 }
