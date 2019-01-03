e4c39d084485c98e1c9bd7766aedf3b36b76964e
Ricardo <ricardo.bragana@edu.unifor.br>
2019-01-03 16:01:22 -0300

    Adding FloatingButton on MaterialIcon and the NinePatchSample

--- src/main/java/totalcross/sample/components/ui/MaterialIconsSample.java
+++ src/main/java/totalcross/sample/components/ui/MaterialIconsSample.java
@@ -1,10 +1,15 @@
 package totalcross.sample.components.ui;
 
+import totalcross.io.IOException;
 import totalcross.sys.Settings;
 import totalcross.ui.Container;
+import totalcross.ui.FloatingButton;
 import totalcross.ui.ScrollContainer;
+import totalcross.ui.gfx.Color;
 import totalcross.ui.icon.Icon;
 import totalcross.ui.icon.MaterialIcons;
+import totalcross.ui.image.Image;
+import totalcross.ui.image.ImageException;
 
 public class MaterialIconsSample extends ScrollContainer {
 	static final int ICON_WIDTH = 64;
@@ -25,7 +30,7 @@ public class MaterialIconsSample extends ScrollContainer {
 		          public void initUI() {
 		            for (int i = 0, j = 0; i < MaterialIcons.values().length; i++, j++) {
 		              Icon icon = new Icon(MaterialIcons.values()[i]);
-		              icon.setFont(icon.getFont());
+		              icon.setFont(icon.getFont().adjustedBy(10));
 		              add(
 		                  icon,
 		                  (j % cols) == 0 ? LEFT : AFTER,
@@ -37,5 +42,22 @@ public class MaterialIconsSample extends ScrollContainer {
 		          }
 		        };
 		    add(c, CENTER, TOP + 100, (int) (cols * ICON_WIDTH * Settings.screenDensity), WILL_RESIZE);
+		    Image ic = null;
+            try {
+                ic = new Image("images/floatbtn.png");
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (ImageException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            FloatingButton floatbutton = new FloatingButton(ic);
+            floatbutton.setBackColor(Color.getRGB(109, 156, 232));
+            floatbutton.setIconSize(30);
+            floatbutton.addPressListener(e -> {
+            	scrollToControl(c);
+            });
+            add(floatbutton, RIGHT-40, BOTTOM-40);
 		  }
 }
