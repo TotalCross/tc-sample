ddb813ca7ad0b3bd7d003abc278a1427ca879b55
AkiraCaio <caio_vini2006@hotmai.com>
2019-01-30 16:55:02 -0300

    Update CommandLine Screen

--- src/main/java/totalcross/sample/components/sys/CommandLine.java
+++ src/main/java/totalcross/sample/components/sys/CommandLine.java
@@ -5,45 +5,61 @@ import totalcross.ui.Button;
 import totalcross.ui.Container;
 import totalcross.ui.Edit;
 import totalcross.ui.Label;
+import totalcross.ui.Radio;
+import totalcross.ui.RadioGroupController;
 import totalcross.ui.gfx.Color;
 import totalcross.util.UnitsConverter;
 
 public class CommandLine extends Container {
 
 	@Override
-	public void initUI() {
-		Label label = new Label("Send terminal commands to the underlying system");
+	public void initUI() {		
+		Label label = new Label("Send terminal commands \nto the underlying system");
+		label.autoSplit = true;
 		label.setForeColor(Color.BLACK);
-		add(label,CENTER,TOP);
+		add(label, CENTER, TOP + UnitsConverter.toPixels(DP + 60));
 
 		Edit editCommand = new Edit();
 		editCommand.caption = "Command";
 		editCommand.fillColor = Color.BRIGHT;
-		add(editCommand,SAME,AFTER + UnitsConverter.toPixels(DP + 100));
+		add(editCommand, CENTER, AFTER + UnitsConverter.toPixels(DP + 60));
 
 		Edit editArgs = new Edit();
 		editArgs.caption = "Args";
 		editArgs.fillColor = Color.BRIGHT;
-		add(editArgs,SAME,AFTER + UnitsConverter.toPixels(DP + 100));
+		add(editArgs, CENTER, AFTER + UnitsConverter.toPixels(DP + 60));
 
 		Edit editLaunch = new Edit();
 		editLaunch.caption = "LaunchCode";
 		editLaunch.fillColor = Color.BRIGHT;
 		editLaunch.setMode(Edit.CURRENCY);
 		editLaunch.setKeyboard(Edit.KBD_NUMERIC);
-		add(editLaunch,SAME,AFTER + UnitsConverter.toPixels(DP + 100));
-
-		Edit editWait = new Edit();
-		editWait.caption = "Wait - true/false";
-		editWait.fillColor = Color.BRIGHT;
-		add(editWait,SAME,AFTER + UnitsConverter.toPixels(DP + 100));
-
+		add(editLaunch, CENTER, AFTER + UnitsConverter.toPixels(DP + 60));
+		
+		Container radioContainer = new Container();
+		add(radioContainer, CENTER, AFTER + UnitsConverter.toPixels(DP + 60), UnitsConverter.toPixels(DP + 200), UnitsConverter.toPixels(DP + 100));
+		
+		RadioGroupController radioGroup = new RadioGroupController();
+		
+		Label labelRadio = new Label("Wait");
+		labelRadio.setForeColor(Color.BLACK);
+		radioContainer.add(labelRadio, CENTER, TOP);
+		
+		Radio radioYes = new Radio("Yes", radioGroup);
+		radioYes.setForeColor(Color.BLACK);
+		radioContainer.add(radioYes, LEFT + UnitsConverter.toPixels(DP + 50), AFTER + UnitsConverter.toPixels(DP + 60));
+		
+		Radio radioNo = new Radio("No", radioGroup);
+		radioNo.setChecked(true);
+		radioNo.setForeColor(Color.BLACK);
+		radioContainer.add(radioNo, AFTER + UnitsConverter.toPixels(DP + 200), SAME);
+			
 		Button btnExe = new Button("Run");
 		btnExe.setForeColor(Color.BLACK);
-		add(btnExe,SAME,AFTER + UnitsConverter.toPixels(DP + 100));
-
+		add(btnExe, CENTER, AFTER);
+		
 		btnExe.addPressListener(e -> {
-			Vm.exec(editCommand.getText(), editArgs.getText(), Integer.parseInt(editLaunch.getText()), Boolean.parseBoolean(editWait.getText()));
+			Vm.exec(editCommand.getText(), editArgs.getText(), Integer.parseInt(editLaunch.getText()), radioYes.isChecked() ? true : false);
 		});
 
 	}
