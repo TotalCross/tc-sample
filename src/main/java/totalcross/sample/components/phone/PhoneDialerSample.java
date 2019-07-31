c5d756755c3d70388abb14e7790d098d2d0e2e88
italo yeltsin <br.yeltsin@gmail.com>
2019-07-31 10:35:00 -0300

    Phone dialer sample now uses HBox and is free of bugs

--- src/main/java/totalcross/sample/components/phone/PhoneDialerSample.java
+++ src/main/java/totalcross/sample/components/phone/PhoneDialerSample.java
@@ -18,25 +18,35 @@ package totalcross.sample.components.phone;
 
 import totalcross.io.IOException;
 import totalcross.phone.Dial;
+import totalcross.sample.components.layout.CalculatorHBoxVBoxSample;
 import totalcross.sample.util.Colors;
+import totalcross.sample.util.MaterialConstants;
 import totalcross.ui.Button;
 import totalcross.ui.Container;
 import totalcross.ui.Edit;
 import totalcross.ui.PushButtonGroup;
 import totalcross.ui.Spacer;
 import totalcross.ui.dialog.MessageBox;
+import totalcross.ui.dialog.keyboard.Builder;
 import totalcross.ui.event.ControlEvent;
 import totalcross.ui.event.Event;
+import totalcross.ui.event.PressListener;
 import totalcross.ui.font.Font;
+import totalcross.ui.layout.HBox;
+import totalcross.ui.layout.VBox;
+import totalcross.util.UnitsConverter;
 
 public class PhoneDialerSample extends Container {
-  private PushButtonGroup pbg;
+  VBox vboxAll;
   private  Edit ed;
   private Button dial;
 
   @Override
   public void initUI() {
     super.initUI();
+    int margin = MaterialConstants.EIGHT_DP_SPACING;
+    setInsets(margin, margin, margin, margin);
+
     setBackForeColors(Colors.BACKGROUND, Colors.ON_BACKGROUND);
     Font big = Font.getFont(true, Font.NORMAL_SIZE + 2);
     ed = new Edit();
@@ -47,43 +57,95 @@ public class PhoneDialerSample extends Container {
     
     add(new Spacer(),LEFT, AFTER, 1, SCREENSIZE+5);
 
-    pbg = new PushButtonGroup(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "P", "0", " << " }, 5, 4);
-    pbg.setFont(big);
-    add(pbg, CENTER, AFTER, SCREENSIZE + 80, SCREENSIZE+65);
-    pbg.setFocusLess(true);
+//    pbg = new PushButtonGroup(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "P", "0", " << " }, 5, 4);
+//    pbg.setFont(big);
+//    add(pbg, CENTER, AFTER, SCREENSIZE + 80, SCREENSIZE+65);
+//    pbg.setFocusLess(true);
     add(new Spacer(),LEFT, AFTER, 1, SCREENSIZE+2);
     dial = new Button("Dial");
     dial.setFont(big);
-    add(dial, CENTER, AFTER, SCREENSIZE+30, SCREENSIZE+10);
     dial.setBackForeColors(Colors.P_600, Colors.ON_P_600);
+
+    vboxAll = new VBox(VBox.LAYOUT_FILL, VBox.ALIGNMENT_STRETCH);
+    vboxAll.setSpacing(2);
+
+    HBox[] rows = new HBox[4];
+    for (int i = 0; i < rows.length; i++) {
+      rows[i] = new HBox(HBox.LAYOUT_FILL, HBox.ALIGNMENT_STRETCH);
+      rows[i].setSpacing(MaterialConstants.TWO_DP_SPACING);
+      vboxAll.add(rows[i]);
+    }
+
+    for (int i = 0; i < rows.length-1; i++) { //Add numbers
+      for (int j = 0; j < 3; j++) { // 1 2 3
+        int number = i*3 + j + 1;
+        rows[i].add(
+                new ButtonBuilder("" + number)
+                .addPressListener((c) -> ed.setText(ed.getText() + number))
+                .build()
+        );
+      }
+    }
+
+    //Last rows
+    rows[3].add(
+            new ButtonBuilder("P")
+                    .addPressListener((c) -> ed.setText(ed.getText() + "P"))
+                    .build()
+    );
+    rows[3].add(
+            new ButtonBuilder("0")
+            .addPressListener((c) -> ed.setText(ed.getText() + "0"))
+            .build()
+    );
+    rows[3].add(
+            new ButtonBuilder("<<")
+            .addPressListener((c) -> {
+              if (ed.getLength() > 0) {
+                ed.setText(ed.getText().substring(0, ed.getText().length() - 1));
+              }
+            })
+            .build()
+    );
+
+    int dialButtonHeight = UnitsConverter.toPixels(DP + 64);
+    this.add(vboxAll, CENTER, AFTER, PARENTSIZE,
+            FILL - dialButtonHeight - MaterialConstants.EIGHT_DP_SPACING);
+
+    dial.addPressListener((c) -> { // Call
+      try {
+        Dial.number(ed.getText());
+      } catch (IOException ex) {
+        MessageBox.showException(ex, true);
+      }
+    });
+    add(dial, CENTER, AFTER + margin, PARENTSIZE, FILL);
+
+
   }
 
   @Override
   public void onEvent(Event e) {
     switch (e.type) {
     case ControlEvent.PRESSED:
-      if (e.target == pbg) {
-        switch (pbg.getSelectedIndex()) {
-        case 11:
-          if (ed.getLength() > 0) {
-            ed.setText(ed.getText().substring(0, ed.getText().length() - 1));
-          }
-          break;
-        case -1:
-          break;
-        default:
-          String s = pbg.getSelectedItem();
-          ed.setText(ed.getText() + s);
-          break;
-        }
+      if (e.target == vboxAll && dial.isEnabled()) {
         dial.setEnabled(ed.getLength() > 0);
-      } else if (e.target == dial) {
-        try {
-          Dial.number(ed.getText());
-        } catch (IOException ex) {
-          MessageBox.showException(ex, true);
-        }
       }
     }
+  }
+
+  private class ButtonBuilder {
+    Button instance;
+
+    ButtonBuilder(String text) {
+      instance = new Button(text);
+    }
+
+    ButtonBuilder addPressListener(PressListener p) {
+      instance.addPressListener(p);
+      return this;
+    }
+
+    public Button build() {return instance;}
   }
 }
