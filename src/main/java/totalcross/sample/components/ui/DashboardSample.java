d986f0b011ef508c12a1a03723ef7df4a10ca26e
Fabio <flsobral@gmail.com>
2019-11-01 22:31:09 -0300

    adds buttons to sections and adds some spacing between them

--- src/main/java/totalcross/sample/components/ui/DashboardSample.java
+++ src/main/java/totalcross/sample/components/ui/DashboardSample.java
@@ -3,40 +3,47 @@ package totalcross.sample.components.ui;
 import totalcross.sample.components.ui.charts.donut.DonutChart;
 import totalcross.sample.components.ui.charts.donut.Section;
 import totalcross.ui.Container;
+import totalcross.ui.Spacer;
 import totalcross.ui.gfx.Color;
+import totalcross.util.UnitsConverter;
 
 public class DashboardSample extends Container {
-    
-    
+
     @Override
     public void initUI() {
         this.setBackColor(Color.getRGB(24, 28, 40));
-        
+
         add(new Container() {
             @Override
             public void initUI() {
                 this.setBackColor(Color.getRGB(41, 45, 57));
             }
-            
+
         }, LEFT, TOP, PARENTSIZE, DP + 56);
-        
+
         add(new Container() {
             @Override
             public void initUI() {
-                
+
             }
-            
+
         }, LEFT, AFTER, PARENTSIZE, DP + 56);
-        
+
         add(new Container() {
             @Override
             public void initUI() {
-                add(new Section("Donut", new DonutChart()), LEFT, AFTER, PARENTSIZE + 40, PARENTSIZE + 50);
-                add(new Section("Values", null), AFTER, SAME, PARENTSIZE + 30, SAME);
-                add(new Section("Gauge", null), AFTER, SAME, PARENTSIZE + 30, SAME);
-                
-                add(new Section("Columns", null), LEFT, AFTER, PARENTSIZE + 70, PARENTSIZE + 50);
-                add(new Section("WTE", null), AFTER, SAME, PARENTSIZE + 30, SAME);
+                final int GAP = UnitsConverter.toPixels(DP + 8);
+
+                Spacer center = new Spacer();
+                add(center, CENTER, CENTER);
+
+                add(new Section("Gauge", null), RIGHT - GAP, TOP, PARENTSIZE + 30, FIT - GAP);
+                add(new Section("Values", null), BEFORE - GAP, SAME, PARENTSIZE + 30, SAME);
+                add(new Section("Donut", new DonutChart()), LEFT + GAP, SAME, FIT - GAP, SAME);
+
+                add(new Section("WTE", null), RIGHT - GAP, AFTER, PARENTSIZE + 30, FILL - GAP, center);
+                add(new Section("Columns", null), LEFT + GAP, SAME, FIT - GAP, SAME);
+
             }
         }, LEFT, AFTER, FILL, FILL);
     }
--- src/main/java/totalcross/sample/components/ui/charts/donut/Section.java
+++ src/main/java/totalcross/sample/components/ui/charts/donut/Section.java
@@ -1,9 +1,14 @@
 package totalcross.sample.components.ui.charts.donut;
 
+import totalcross.ui.Button;
 import totalcross.ui.Container;
 import totalcross.ui.Control;
 import totalcross.ui.Label;
+import totalcross.ui.Ruler;
+import totalcross.ui.font.Font;
 import totalcross.ui.gfx.Color;
+import totalcross.ui.icon.MaterialIcons;
+import totalcross.util.UnitsConverter;
 
 public class Section extends Container {
     
@@ -17,9 +22,30 @@ public class Section extends Container {
 
     @Override
     public void initUI() {
+        this.borderColor = Color.getRGB(47, 47, 47);
+        this.setBorderStyle(BORDER_ROUNDED);
         this.setBackColor(Color.getRGB(47, 47, 47));
         
-        add(new Label(title), LEFT, TOP, FILL, DP + 36);
+        Button btMore = new Button(String.valueOf(MaterialIcons._MORE_VERT), Button.BORDER_NONE);
+        btMore.setForeColor(Color.getRGB(127, 137, 153));
+        btMore.transparentBackground = true;
+        btMore.setFont(Font.getFont(MaterialIcons._3D_ROTATION.fontName(), false, 18));
+        add(btMore, RIGHT, TOP, PREFERRED, PREFERRED);
+        
+        Button btConfig = new Button(String.valueOf(MaterialIcons._SETTINGS), Button.BORDER_NONE);
+        btConfig.setForeColor(Color.getRGB(127, 137, 153));
+        btConfig.transparentBackground = true;
+        btConfig.setFont(Font.getFont(MaterialIcons._3D_ROTATION.fontName(), false, 18));
+        add(btConfig, BEFORE, TOP, PREFERRED, PREFERRED);
+        
+        Label lblTitle = new Label(title);
+        lblTitle.setForeColor(Color.getRGB(127, 137, 153));
+        lblTitle.setFont(Font.getFont(18));
+        add(lblTitle, LEFT + UnitsConverter.toPixels(DP + 10), TOP, FIT, SAME);
+        
+        Ruler r = new Ruler();
+        r.setForeColor(Color.getRGB(127, 137, 153));
+        add(r, LEFT, AFTER, FILL, PREFERRED);
         
         if (this.content != null) {
             add(content, LEFT, AFTER, FILL, FILL);
