975e67a31be39f8f9eb4e486e968c6b1c0ff886c
Fabio <flsobral@gmail.com>
2019-11-02 17:50:39 -0300

    reduces frame rate by repainting only every third update

--- src/main/java/totalcross/sample/components/ui/charts/gauge/GaugeSample.java
+++ src/main/java/totalcross/sample/components/ui/charts/gauge/GaugeSample.java
@@ -1,25 +1,21 @@
 package totalcross.sample.components.ui.charts.gauge;
 
-
 import totalcross.sample.components.ui.charts.AnimationFunctions;
-import totalcross.sys.Vm;
 import totalcross.ui.Container;
 import totalcross.ui.MainWindow;
 import totalcross.ui.event.UpdateListener;
 import totalcross.ui.font.Font;
-import totalcross.util.UnitsConverter;
 
 public class GaugeSample extends Container {
-    public GaugeSample() {
 
-    }
     UpdateListener updateListener;
+
     @Override
     public void initUI() {
         int Color1 = 0x2ECC40;
         int Color2 = 0xFFDC00;
         int Color3 = 0xFF4136;
-        
+
         Gauge energyGauge = new EnergyGauge(0, 80, 180);
         energyGauge.section(80, Color1);
         energyGauge.section(80, Color2);
@@ -30,7 +26,7 @@ public class GaugeSample extends Container {
         energyGauge.setValueSuffix(" °C");
         energyGauge.setForeColor(0x0);
         energyGauge.setValue(60);
-        
+
         add(energyGauge, CENTER, TOP, PARENTSIZE + 33, PARENTSIZE);
 
         Gauge stylishSectionsGauge = new StylishSectionsGauge(0, 180, 270);
@@ -54,41 +50,41 @@ public class GaugeSample extends Container {
 
         add(storageGauge, AFTER, SAME, FILL, SAME, energyGauge);
 
-        updateListener  = new UpdateListener() {
+        updateListener = new UpdateListener() {
             AnimationFunctions af = new AnimationFunctions();
             int rpmTotalTime = 3000;
             int currentime = 0;
+
+            int count = -1;
+
             @Override
             public void updateListenerTriggered(int elapsedMilliseconds) {
                 currentime += elapsedMilliseconds;
-                double ratio = rpmAnimationFunction(currentime%rpmTotalTime, rpmTotalTime);
-                stylishSectionsGauge
-                        .setValue((int)((stylishSectionsGauge.max - stylishSectionsGauge.min)*ratio)
-                                - stylishSectionsGauge.min);
-                stylishSectionsGauge.repaintNow();
 
-                energyGauge
-                        .setValue((int)((energyGauge.max - energyGauge.min)*ratio)
-                                - energyGauge.min);
-                energyGauge.repaintNow();
+                if (++count % 3 == 0) {
+                    double ratio = rpmAnimationFunction(currentime % rpmTotalTime, rpmTotalTime);
+                    stylishSectionsGauge.setValue((int) ((stylishSectionsGauge.max - stylishSectionsGauge.min) * ratio)
+                            - stylishSectionsGauge.min);
+                    stylishSectionsGauge.repaintNow();
+
+                    energyGauge.setValue((int) ((energyGauge.max - energyGauge.min) * ratio) - energyGauge.min);
+                    energyGauge.repaintNow();
+                }
             }
 
             public double rpmAnimationFunction(int time, int totalTime) {
-                double ratio = time/(double)totalTime;
-                if(ratio < .30) {
-                    ratio = ratio/0.30;
+                double ratio = time / (double) totalTime;
+                if (ratio < .30) {
+                    ratio = ratio / 0.30;
                     return af.easeOutQuad(ratio);
-                }
-                else {
-                    ratio = (ratio - 0.30)/0.70;
+                } else {
+                    ratio = (ratio - 0.30) / 0.70;
                     return 1.0 - af.easeOutSine(ratio);
                 }
             }
         };
         MainWindow.getMainWindow().addUpdateListener(updateListener);
     }
-
-
 
     @Override
     protected void onRemove() {
