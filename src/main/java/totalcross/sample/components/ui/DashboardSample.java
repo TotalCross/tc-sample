45da1b626ecf0a9ab18eb0b1f8262ec335bc5d47
italo yeltsin <br.yeltsin@gmail.com>
2019-11-02 20:01:10 -0300

    quality metrics dashboard

--- src/main/java/totalcross/sample/components/ui/DashboardSample.java
+++ src/main/java/totalcross/sample/components/ui/DashboardSample.java
@@ -4,9 +4,13 @@ import totalcross.sample.components.ui.charts.column.DashboardColumnChart;
 import totalcross.sample.components.ui.charts.donut.DonutChart;
 import totalcross.sample.components.ui.charts.donut.Section;
 import totalcross.sample.components.ui.charts.gauge.GaugeSample;
-import totalcross.ui.Container;
-import totalcross.ui.Spacer;
+import totalcross.ui.*;
+import totalcross.ui.font.Font;
 import totalcross.ui.gfx.Color;
+import totalcross.ui.gfx.Rect;
+import totalcross.ui.layout.HBox;
+import totalcross.ui.layout.LinearBox;
+import totalcross.ui.layout.VBox;
 import totalcross.util.UnitsConverter;
 
 public class DashboardSample extends Container {
@@ -34,7 +38,85 @@ public class DashboardSample extends Container {
                 add(new Section("Gauge", new GaugeSample()), RIGHT - GAP, TOP, PARENTSIZE + 60, FIT - GAP);
                 add(new Section("Donut", new DonutChart()), LEFT + GAP, SAME, FIT - GAP, SAME);
 
-                add(new Section("WTE", null), RIGHT - GAP, AFTER, PARENTSIZE + 30, FILL - GAP, center);
+                add(new Section("Quality Metrics", new Container() {
+                    @Override
+                    public void initUI() {
+                        setBackColor(getParent().getBackColor());
+                        HBox allBox = new HBox();
+                        allBox.transparentBackground = true;
+                        allBox.setSpacing(UnitsConverter.toPixels(DP + 12));
+                        allBox.setLayout(LinearBox.LAYOUT_STACK_CENTER, LinearBox.LAYOUT_STACK_CENTER);
+
+                        LinearBox lbBox1 = createBox(false);
+                        lbBox1.add(new Control[]{
+                                createLb("MAF"),
+                                createLb("MHF"),
+                                createLb("MIF"),
+                                createLb("AIF"),
+                                createLb("CF"),
+                        });
+
+                        LinearBox valueBox1 = createBox(false);
+                        valueBox1.add(new Control[]{
+                                createValLb("13.3%"),
+                                createValLb("84.1%"),
+                                createValLb("63.1%"),
+                                createValLb("62.6%"),
+                                createValLb("2.8%"),
+                        });
+
+                        LinearBox lbBox2 = createBox(false);
+                        lbBox2.add(new Control[]{
+                                createLb("CBO"),
+                                createLb("LCOM"),
+                                createLb("DOIH"),
+                                createLb("LOC"),
+                                createLb("ESPQ"),
+
+                        });
+
+                        LinearBox valueBox2 = createBox(false);
+                        valueBox2.add(new Control[]{
+                                createValLb("33%"),
+                                createValLb("20%"),
+                                createValLb("67.07%"),
+                                createValLb("40.13%"),
+                                createValLb("8%"),
+                        });
+                        Spacer s = new Spacer();
+                        s.setRect(new Rect(0, 0, UnitsConverter.toPixels(DP + 24), 1));
+                        allBox.add(new Control[]{lbBox1, valueBox1,  s,lbBox2, valueBox2});
+                        add(allBox, LEFT, TOP, PARENTSIZE, PARENTSIZE);
+
+                    }
+
+                    public LinearBox createBox(boolean isHBox) {
+                        LinearBox lbox = isHBox? new HBox(): new VBox();
+                        lbox.setLayout(VBox.LAYOUT_DISTRIBUTE, VBox.ALIGNMENT_LEFT);
+                        lbox.setSpacing(UnitsConverter.toPixels(DP + 8));
+                        return lbox;
+                    }
+
+                    public Label createLb(String text) {
+                        Font lbFont = Font.getFont(12);
+                        Label l = new Label(text);
+                        l.transparentBackground = true;
+                        l.setFont(lbFont);
+                        l.setForeColor(Color.BRIGHT);
+                        return  l;
+                    }
+
+                    public Label createValLb(String text) {
+                        Font valueFont = Font.getFont("Lato Bold", false,13);
+                        Label l = new Label(text);
+                        l.transparentBackground = true;
+                        l.setForeColor(Color.GREEN);
+                        l.setFont(valueFont);
+                        return  l;
+                    }
+
+
+                }), RIGHT - GAP, AFTER, PARENTSIZE + 30, FILL - GAP, center);
                 add(new Section("Columns", new DashboardColumnChart()), LEFT + GAP, SAME, FIT - GAP, SAME);
 
             }
