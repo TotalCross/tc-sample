2dbbc481279c79f9914422c3394d30a1e0e87bf6
Fabio <flsobral@gmail.com>
2019-11-01 23:43:10 -0300

    reorders and resizes charts in sample

--- src/main/java/totalcross/sample/components/ui/charts/gauge/GaugeSample.java
+++ src/main/java/totalcross/sample/components/ui/charts/gauge/GaugeSample.java
@@ -15,16 +15,19 @@ public class GaugeSample extends Container {
         int Color1 = 0x2ECC40;
         int Color2 = 0xFFDC00;
         int Color3 = 0xFF4136;
-
-        Gauge storageGauge = new StorageGauge(0, 180, 270);
-
-        storageGauge.setForeColor(0xF35C00);
-        storageGauge.showTickValues = true;
-        storageGauge.showTicks = true;
-        storageGauge.showTickValues(false);
-        storageGauge.setValue(80);
-
-        add(storageGauge, CENTER, TOP, PARENTSIZE + 33, PARENTSIZE + 50);
+        
+        Gauge energyGauge = new EnergyGauge(0, 80, 180);
+        energyGauge.section(80, Color1);
+        energyGauge.section(80, Color2);
+        energyGauge.section(80, Color3);
+        energyGauge.setMajorTickInterval(20);
+        energyGauge.setMinorTickInterval(5);
+        energyGauge.setValueLabelFont(Font.getFont(25));
+        energyGauge.setValueSuffix(" °C");
+        energyGauge.setForeColor(0x0);
+        energyGauge.setValue(60);
+        
+        add(energyGauge, CENTER, TOP, PARENTSIZE + 50, PARENTSIZE + 50);
 
         Gauge stylishSectionsGauge = new StylishSectionsGauge(0, 180, 270);
         stylishSectionsGauge.section(80, Color1);
@@ -35,19 +38,16 @@ public class GaugeSample extends Container {
         stylishSectionsGauge.setForeColor(0x0);
         stylishSectionsGauge.setValue(80);
 
-        add(stylishSectionsGauge, LEFT, AFTER, PARENTSIZE + 33, PARENTSIZE + 50);
+        add(stylishSectionsGauge, LEFT, AFTER, PARENTSIZE + 50, PARENTSIZE + 50);
 
-        Gauge energyGauge = new Gauge(0, 80, 180);
-        energyGauge.section(80, Color1);
-        energyGauge.section(80, Color2);
-        energyGauge.section(80, Color3);
-        energyGauge.setMajorTickInterval(20);
-        energyGauge.setMinorTickInterval(5);
-        energyGauge.setValueLabelFont(Font.getFont(25));
-        energyGauge.setValueSuffix(" °C");
-        energyGauge.setForeColor(0x0);
-        energyGauge.setValue(60);
+        Gauge storageGauge = new StorageGauge(0, 180, 270);
 
-        add(energyGauge, RIGHT, AFTER, PARENTSIZE + 33, PARENTSIZE + 50);
+        storageGauge.setForeColor(0xF35C00);
+        storageGauge.showTickValues = true;
+        storageGauge.showTicks = true;
+        storageGauge.showTickValues(false);
+        storageGauge.setValue(80);
+
+        add(storageGauge, RIGHT, SAME, PARENTSIZE + 50, PARENTSIZE + 50);
     }
 }
