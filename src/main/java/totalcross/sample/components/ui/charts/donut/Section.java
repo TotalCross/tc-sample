00ab3e23342b34c0968f6244cd8fec9987d21764
Fabio <flsobral@gmail.com>
2019-11-04 22:48:27 -0300

    reduces font size and fixes buttons being too large

--- src/main/java/totalcross/sample/components/ui/charts/donut/Section.java
+++ src/main/java/totalcross/sample/components/ui/charts/donut/Section.java
@@ -15,6 +15,8 @@ public class Section extends Container {
     String title;
     Control content;
     
+    final static int FONT_SIZE = 15;
+    
     public Section(String title, Control content) {
         this.title = title;
         this.content = content;
@@ -27,18 +29,18 @@ public class Section extends Container {
         Button btMore = new Button(String.valueOf(MaterialIcons._MORE_VERT), Button.BORDER_NONE);
         btMore.setForeColor(Color.getRGB(127, 137, 153));
         btMore.transparentBackground = true;
-        btMore.setFont(Font.getFont(MaterialIcons._3D_ROTATION.fontName(), false, 18));
-        add(btMore, RIGHT, TOP, PREFERRED, PREFERRED);
+        btMore.setFont(Font.getFont(MaterialIcons._3D_ROTATION.fontName(), false, FONT_SIZE));
+        add(btMore, RIGHT, TOP, DP + 32, PREFERRED);
         
         Button btConfig = new Button(String.valueOf(MaterialIcons._SETTINGS), Button.BORDER_NONE);
         btConfig.setForeColor(Color.getRGB(127, 137, 153));
         btConfig.transparentBackground = true;
-        btConfig.setFont(Font.getFont(MaterialIcons._3D_ROTATION.fontName(), false, 18));
-        add(btConfig, BEFORE, TOP, PREFERRED, PREFERRED);
+        btConfig.setFont(Font.getFont(MaterialIcons._3D_ROTATION.fontName(), false, FONT_SIZE));
+        add(btConfig, BEFORE, TOP, DP + 32, PREFERRED);
         
         Label lblTitle = new Label(title);
         lblTitle.setForeColor(Color.getRGB(127, 137, 153));
-        lblTitle.setFont(Font.getFont(18));
+        lblTitle.setFont(Font.getFont(FONT_SIZE));
         add(lblTitle, LEFT + UnitsConverter.toPixels(DP + 10), TOP, FIT, SAME);
         
         Ruler r = new Ruler();
